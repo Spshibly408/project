a67b5ea820edc6d9fafc06705077af5aee4acb2e
Md. Shibly <17677343+Spshibly408@users.noreply.github.com>
2017-10-10 18:35:11 +0600

    Add files via upload

---
+++ index.php
@@ -0,0 +1,38 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <title>Document</title>
+</head>
+
+<body>
+    <?php
+
+        $sp='spark';
+
+        if($sp=='spark') {
+            echo "Whats up $sp Bro ?";
+            } 
+        else{
+            echo "who are you $sp ?";
+            }
+//    
+//    $friends= array('Sojon','Pappu','Tapos','Rayhan')
+//        echo $friends[2] 
+echo ' <br> <br> <br> <br> <br> <br> <br> <br>';
+    
+    $abc= 'Hello';
+    $abc2='Friends';
+    if($abc==$abc2){
+        echo " value Match";
+    }
+    else{
+        echo "value not Match";
+    }
+?>
+
+
+</body>
+
+</html>
